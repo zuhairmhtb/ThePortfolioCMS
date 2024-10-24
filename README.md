7f78d19e0cd20bf4b592eac2f18160cd7657a092
zuhairmhtb <zuhairmhtb@gmail.com>
2024-10-24 23:55:05 +0600

    First commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# My Portfolio - The Frontend Experience (Wordpress CMS)
+
+This repository contains the application that will act as the Rest API to the frontend application (Angular). I have used Wordpress as the CMS because it provides a rich editor to create posts like documentations. To run the application - just run the docker compose file!
+
+
---
+++ build-tools/deployment.sh
@@ -0,0 +1,81 @@
+#!/bin/bash
+
+while getopts i:u:m:s:d:r flag
+do
+    case "${flag}" in
+        i) server_ip=${OPTARG};; # IP address of the server
+        u) server_username=${OPTARG};; # SSH user of the server
+        m) mode=${OPTARG};; # prod/staging
+        s) ssh_key=${OPTARG};; # ssh key
+        d) base_directory=${OPTARG};; # base directory
+        r) serverBaseDirectory=${OPTARG};; # server base directory
+    esac
+done
+
+if [ -z $mode ];
+then
+    echo "Please provide application mode using parameter '-m'. The mode can either be 'prod' or 'staging'"
+    exit 1
+fi
+
+if [ -z $server_ip ];
+then
+    echo "Please provide ip address of the server using the parameter '-i'"
+    exit 1
+fi
+
+if [ -z $server_username ];
+then
+    echo "Please provide the ssh username of the server using the parameter '-u'"
+    exit 1
+fi
+
+if [ -z $ssh_key ];
+then
+    echo "Please provide the ssh key path using the parameter '-s'"
+    exit 1
+fi
+
+if [ -z $base_directory ];
+then
+    echo "Please provide the base directory path using the parameter '-d'"
+    exit 1
+fi
+
+echo "Running deployment script for application mode '$mode' from directory '$base_directory' to server '$server_username@$server_ip'"
+# Server variables
+server="$server_username@$server_ip" # Server info
+localBaseDirectory=$base_directory # Base directory on the local machine
+
+echo "Deploying application to server $server"
+
+# 1. Stop the docker container in ~/iam directory
+echo "Stopping the docker container"
+ssh -i $ssh_key $server "cd $serverBaseDirectory && docker compose down"
+
+# 2. Copy the docker compose directory to the server
+echo "Copying the docker compose file to the server"
+scp -i $ssh_key $localBaseDirectory/docker-compose.production.yml  $server:$serverBaseDirectory/docker-compose.temp.yml
+
+# 3. Copy the wp-content directory of wordpress
+echo "Copying the wp-content directory to the server"
+scp -i $ssh_key -r $localBaseDirectory/app/wp-content $server:$serverBaseDirectory/app/wp-content
+
+
+echo "Moving the docker-compose file to the correct location"
+ssh -i $ssh_key $server "sudo mv $serverBaseDirectory/docker-compose.temp.yml $serverBaseDirectory/docker-compose.yml"
+
+# Copy env file if it exists
+if [ -f "$localBaseDirectory/.env" ]; then
+    echo "Copying .env file from $localBaseDirectory to $serverBaseDirectory"
+    scp -i $ssh_key $localBaseDirectory/.env $server:$serverBaseDirectory/.env
+else
+    echo ".env file does not exist in $base_directory"
+fi
+
+# 4. Start the docker container in ~/iam directory
+echo "Starting the docker container"
+ssh -i $ssh_key $server "cd $serverBaseDirectory && docker compose up -d"
+
+
+
